fbbb6bf4c5bcdfb60954b531a4df596dec7d1479
Aaditya Padte <aadityajp0419@gmail.com>
2023-10-21 15:42:44 +0530

    features_in_progress

--- components/Landing/Features.jsx
+++ components/Landing/Features.jsx
@@ -2,28 +2,51 @@ import Image from "next/image";
 import React, { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import Lottie from "react-lottie";
 
 const Features = ({ data }) => {
   useEffect(() => {
     Aos.init();
   }, []);
   return (
-    <div className="flex flex-col lg:flex-row gap-20 even:lg:flex-row-reverse ">
-      <div className="flex flex-col gap-6" data-aos="fade-right">
+    <div className="flex flex-col lg:flex-row gap-20 even:lg:flex-row-reverse items-center">
+      <div className="flex flex-col gap-6 flex-1" data-aos="fade-right">
         <p className="text-2xl text-blue-900 font-semibold">
           <span className="text-orange-500">{data.mainLabel}</span> {data.label}
         </p>
-        <p>{data.desc}</p>
+        <p className="text-lg">{data.desc}</p>
+
+        {data?.details && (
+          <div className="grid gap-6">
+            {data?.details?.map((item, index) => {
+              const Icon = item.logo;
+              return (
+                <div className="flex gap-4 items-center" key={index}>
+                  <div className="p-3 rounded-full bg-white shadow-lg shrink-0">
+                    <Icon className="h-6 w-6" />
+                  </div>
+                  <p className="shrink-0 text-lg">{item.desc}</p>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
 
-      <Image
-        data-aos="fade-left"
-        src={data.logo.src}
-        width={500}
-        height={500}
-        alt="features"
-        className="flex"
-      />
+      {data.isLottie ? (
+        <div  data-aos="fade-left" className="flex-1">
+          <Lottie options={data.options}/>
+        </div>
+      ) : (
+        <Image
+          data-aos="fade-left"
+          src={data.logo.src}
+          width={500}
+          height={500}
+          alt="features"
+          className="flex-1 aspect-[8/4]"
+        />
+      )}
     </div>
   );
 };
--- components/Landing/Header.jsx
+++ components/Landing/Header.jsx
@@ -38,59 +38,64 @@ const Header = ({ activeSection }) => {
   }, [activeSection]);
 
   return (
-    <motion.header
-      className="flex sticky top-0 z-[999] items-center w-screen lg:px-32 justify-between py-2 h-[80px] lg:h-auto  bg-[#e9f8f5]"
-      initial={{ opacity: 0, y: -100 }}
-      animate={{ opacity: 1, y: 0 }}
-    >
-      <div className="flex gap-4 justify-center items-center">
-        <Image src={StudyLogo} alt="studyNex logo" height={60} width={60} />
-        <p className="font-bold text-xl lg:text-3xl">StudyNex</p>
-      </div>
-      <nav
-        ref={navRef}
-        className="fixed top-0 lg:h-auto h-screen w-screen lg:w-auto text-[20px] bg-[rgba(153,246,220,0.9)] lg:bg-transparent flex items-center justify-center flex-col gap-6 transition duration-[1s] -translate-y-[100vh] lg:translate-y-0 lg:relative lg:flex-row lg:justify-center"
+      <motion.header
+        className="flex sticky top-0 z-[999] items-center w-screen lg:px-32 justify-between py-2 h-[80px] lg:h-auto  bg-[#e9f8f5]"
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
       >
-        {navBarValues.map((item, index) => (
-          <Link
-            activeClass="active"
-            spy={true}
-            smooth={true}
-            offset={-80}
-            duration={500}
-            to={item.link}
-            className={classNames(
-              'mx-[2rem] lg:cursor-pointer relative before:content-[""] before:absolute before:left-0 before:bottom-0 before:w-full before:bg-black before:scale-x-0 before:origin-center before:transform before:duration-[0.3s] before:ease-in-out before:hover:scale-x-[1]',
-              activeSection == item?.link || item?.label == clicked
-                ? "border-b-2 transition-all"
-                : "before:h-[2px]"
-            )}
-            key={index}
-            onClick={() => {
-              setClicked(item?.label);
-              showNavbar(false);
-            }}
-          >
-            {item?.label}
-          </Link>
-        ))}
-
         <div
-          className="absolute top-8 right-8  lg:p-[5px] lg:hidden"
-          onClick={showNavbar}
+          className="flex gap-4 justify-center items-center lg:cursor-pointer"
+          onClick={() => {
+            window.location.reload();
+          }}
         >
-          <FaTimes />
+          <Image src={StudyLogo} alt="studyNex logo" height={60} width={60} />
+          <p className="font-bold text-xl lg:text-3xl">StudyNex</p>
         </div>
-      </nav>
+        <nav
+          ref={navRef}
+          className="fixed top-0 lg:h-auto h-screen w-screen lg:w-auto text-[20px] bg-[rgba(153,246,220,0.9)] lg:bg-transparent flex items-center justify-center flex-col gap-6 transition duration-[1s] -translate-y-[100vh] lg:translate-y-0 lg:relative lg:flex-row lg:justify-center"
+        >
+          {navBarValues.map((item, index) => (
+            <Link
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={500}
+              to={item.link}
+              className={classNames(
+                'mx-[2rem] lg:cursor-pointer relative before:content-[""] before:absolute before:left-0 before:bottom-0 before:w-full before:bg-black before:scale-x-0 before:origin-center before:transform before:duration-[0.3s] before:ease-in-out before:hover:scale-x-[1]',
+                activeSection == item?.link || item?.label == clicked
+                  ? "border-b-2 transition-all"
+                  : "before:h-[2px]"
+              )}
+              key={index}
+              onClick={() => {
+                setClicked(item?.label);
+                showNavbar(false);
+              }}
+            >
+              {item?.label}
+            </Link>
+          ))}
 
-      <div className="hidden lg:flex gap-4 items-center">
-        <PrimaryBtn label="Login" invert />
-        <PrimaryBtn label="Sign Up" />
-      </div>
-      <div className="p-5 lg:hidden" onClick={showNavbar}>
-        <FaBars />
-      </div>
-    </motion.header>
+          <div
+            className="absolute top-8 right-8  lg:p-[5px] lg:hidden"
+            onClick={showNavbar}
+          >
+            <FaTimes />
+          </div>
+        </nav>
+
+        <div className="hidden lg:flex gap-4 items-center">
+          <PrimaryBtn label="Login" invert />
+          <PrimaryBtn label="Sign Up" />
+        </div>
+        <div className="p-5 lg:hidden" onClick={showNavbar}>
+          <FaBars />
+        </div>
+      </motion.header>
   );
 };
 
