6823740403eff567a2cef50ceb62bab4f8f09075
Aaditya Padte <aadityajp0419@gmail.com>
2023-10-21 11:54:53 +0530

    image_compressed

--- components/Constants/imageContants.js
+++ components/Constants/imageContants.js
@@ -1,7 +1,7 @@
 import studyLogo from '../../public/Assets/Images/studyLogo.svg'
 import thumbnailImg from '../../public/Assets/Images/study2.png'
 import thumbnailImg2 from '../../public/Assets/Images/study3.png'
-import teacher1 from '../../public/Assets/Images/teacher1.jpg'
+import teacher1 from '../../public/Assets/Images/teacher.jpg'
 import teacher2 from '../../public/Assets/Images/teacher2.jpg'
 import student1 from '../../public/Assets/Images/student1.jpg'
 
